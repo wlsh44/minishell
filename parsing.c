9bca0d60023c2204c425016e3c08bb9c6d97a183
wlsh44 <k87913j@naver.com>
2021-01-08 22:43:25 +0900

    todo: add sep parsing func, debug

--- parsing.c
+++ parsing.c
@@ -2,13 +2,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define TOO_MANY_ARG -2
+#define WRONG_CMD -5
+#define NOT_VAILD_ARG -4
 #define WRONG_QUOTE -3
+#define TOO_MANY_ARG -2
+#define NOT_ENOUGH_ARG -1
+
 #define TYPE_CD 1
 #define TYPE_PWD 2
 #define TYPE_ECHO 3
 #define TYPE_ECHO_N 4
-
+#define TYPE_EXPORT 5
+#define TYPE_ENV 6
+#define TYPE_UNSET 7
+#define TYPE_PIPE 8
+#define TYPE_REDIRECT 9
+#define TYPE_DOUBLE_REDIRECT 10
+#define TYPE_EXIT 11
+#define TYPE_SEMICOLON 12
 /*
 cd
 echo
@@ -108,7 +119,21 @@ char	*ft_strdup(const char *s)
 	return (arr);
 }
 
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char *str;
+	char *pt;
 
+	if (!(str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
+		return (NULL);
+	pt = str;
+	while (*s1)
+		*pt++ = *s1++;
+	while (*s2)
+		*pt++ = *s2++;
+	*pt = '\0';
+	return (str);
+}
 
 int		ft_strcmp(const char *s1, const char *s2)
 {
@@ -153,6 +178,12 @@ int		ft_isquote(char c)
 	return ((c == '\'' || c == '"'));
 }
 
+int		ft_isdigit(char c)
+{
+	return ((c >= '0' && c <= '9'));
+}
+
+
 int get_arg_num(char *line) {
 	int arg_num;
 	int flag;
@@ -184,17 +215,8 @@ int get_arg_size(char *line) {
 	int size;
 	
 	size = 0;
-	while (ft_isspace(*line))
-		line++;
 	while (*line && !ft_isseparator(*line) && !ft_isspace(*line)) {
-		if (*line == '\\' && (ft_isquote(line + 1) || *(line + 1) == '\\')) {
-			size++;
-			line += 2;
-		} else {
-			if (!ft_isquote(*line))
-				size++;
-			line++;
-		}
+		size++;
 	}
 	return (size);
 }
@@ -206,31 +228,28 @@ int get_arg_char(char **line, char *arg) {
 	flag = 1;
 	while (ft_isspace(**line))
 		(*line)++;
-	while (**line && !ft_isseparator(**line) && !ft_isspace(**line)) {
+	while (*line && !ft_isseparator(*line) && !ft_isspace(*line)) {
 		if (**line == '\\') {
 			(*line)++;
-			if (**line == '\\' || ft_isquote(**line)) {
+			if (**line)
 				*(arg++) = *(*line)++;
-			}
 		} else if (ft_isquote(**line)) {
-			quote = **line;
-			if (**line == quote) {
-				(*line)++;
-				flag = 0;
-				while (**line && !ft_isseparator(**line) && !ft_isspace(**line)) {
-					if (**line != quote) {
-						flag = 1;
-						break;
-					} else if (**line == '\\') {
-						*(arg++) = *(*line)++;
-						while (**line == '\\')
-							(*line)++;
-					} else {
-						*(arg++) = *(*line)++;
-					}
+			quote = *(*line)++;
+			flag = 0;
+			while (**line && !ft_isseparator(**line) && !ft_isspace(**line)) {
+				if (**line == quote) {
+					flag = 1;
+					break;
+				} else if (**line == '\\') {
+					*(arg++) = *(*line)++;
+					while (**line == '\\')
+						(*line)++;
+				} else {
+					*(arg++) = *(*line)++;
 				}
 			}
-		}
+		} else 
+			*(arg++) = *(*line)++;
 	}
 	while (ft_isspace(**line))
 		(*line)++;
@@ -257,7 +276,6 @@ int parsing_cd(t_lstcmd *cmd, char **line) {
 	int ret;
 	char *arg;
 
-	(*line) += 2;
 	if (get_arg_num(*line) > 1)
 		return (TOO_MANY_ARG);
 	if ((ret = get_arg(line, &arg) < 0))
@@ -281,7 +299,6 @@ int parsing_echo(t_lstcmd *cmd, char **line) {
 	int type;
 	char *arg;
 
-	(*line) += 2;
 	type = check_echo_option(line) ? TYPE_ECHO_N : TYPE_ECHO;
 	if (get_arg_num(*line) > 1)
 		return (TOO_MANY_ARG);
@@ -300,59 +317,245 @@ void cmd_error(int err) {
 }
 
 int parsing_pwd(t_lstcmd *cmd, char **line) {
-	(*line) += 3;
 	if (get_arg_num(*line) > 0)
 		return (TOO_MANY_ARG);
+	while (ft_isspace(**line))
+			(*line)++;
 	push_back(cmd, TYPE_PWD, NULL);
+	return (0);
+}
+
+int parsing_env(t_lstcmd *cmd, char **line) {
+	if (get_arg_num(*line) > 0)
+		return (TOO_MANY_ARG);
+	while (ft_isspace(**line))
+			(*line)++;
+	push_back(cmd, TYPE_ENV, NULL);
+	return (0);
+}
+
+int parsing_unset(t_lstcmd *cmd, char **line) {
+	int arg_num;
+	int size;
+	char *arg;
+	char *tmp;
+	int ret;
+
+	while (ft_isspace(**line))
+			(*line)++;
+	if ((arg_num = get_arg_num(*line)) == 0)
+		return (NOT_ENOUGH_ARG);
+	while (arg_num--) {
+		size = get_arg_size(*line);
+		arg = malloc(sizeof(char) * (size + 1));
+		if ((ret = get_arg_export_unset(line, arg)) < 0)
+			return (ret);
+		tmp = ft_strjoin(arg, " ");
+		free(arg);
+		arg = tmp;
+		while (ft_isspace(**line))
+			(*line)++;
+	}
+	push_back(cmd, TYPE_UNSET, arg);
+	return (0);
+}
+
+int get_arg_export_unset(char **line, char *arg) {
+	char quote;
+	int flag;
+
+	flag = 1;
+	while (**line && !ft_isseparator(**line) && !ft_isspace(*line) && **line != '=') {
+		if (ft_isalpha(*line) || ft_isdigit(**line) || **line == '_')
+			*(arg++) = *(*line)++;
+		else if (**line == '\\') {
+			(*line)++;
+			if (**line)
+				*(arg++) = *(*line)++;
+		} else if (ft_isquote(**line)) {
+			quote = *(*line)++;
+			flag = 0;
+			while (**line && !ft_isseparator(**line) && !ft_isspace(**line) && **line != '=') {
+				if (ft_isalpha(*line) || ft_isdigit(**line) || **line == '_')
+					*(arg++) = *(*line)++;
+				else if (**line == quote) {
+					flag = 1;
+					break;
+				} else if (**line == '\\') {
+					*(arg++) = *(*line)++;
+					while (**line == '\\')
+						(*line)++;
+				} else {
+					return (NOT_VAILD_ARG);
+				}
+			}
+		} else 
+			return (NOT_VAILD_ARG);
+	}
+	if (*line == '=' && flag) {
+		*(arg++) = *(*line)++;
+		while (*line && !ft_isseparator(*line) && !ft_isspace(*line)) {
+			if (**line == '\\') {
+				(*line)++;
+				if (**line)
+					*(arg++) = *(*line)++;
+			} else if (ft_isquote(**line)) {
+				quote = *(*line)++;
+				flag = 0;
+				while (**line && !ft_isseparator(**line) && !ft_isspace(**line)) {
+					if (**line == quote) {
+						flag = 1;
+						break;
+					} else if (**line == '\\') {
+						*(arg++) = *(*line)++;
+						while (**line == '\\')
+							(*line)++;
+					} else {
+						*(arg++) = *(*line)++;
+					}
+				}
+			} else 
+				*(arg++) = *(*line)++;
+		}
+	}
+	*arg = '\0';
+	if (!flag)
+		return (WRONG_QUOTE);
 	return (0);
 }
 
 int parsing_export(t_lstcmd *cmd, char **line) {
 	int arg_num;
 	int size;
+	char *arg;
+	char *tmp;
+	int ret;
 
+	while (ft_isspace(**line))
+			(*line)++;
 	arg_num = get_arg_num(*line);
-	while ()
+	while (arg_num--) {
+		size = get_arg_size(*line);
+		arg = malloc(sizeof(char) * (size + 1));
+		if ((ret = get_arg_export_unset(line, arg)) < 0)
+			return (ret);
+		tmp = ft_strjoin(arg, " ");
+		free(arg);
+		arg = tmp;
+		while (ft_isspace(**line))
+			(*line)++;
+	}
+	push_back(cmd, TYPE_EXPORT, arg);
+	return (0);
 }
+
+int parsing_exit(t_lstcmd *cmd, char **line) {
+	while (**line && !ft_isseparator(**line))
+		(*line)++;
+	push_back(cmd, TYPE_EXIT, NULL);
+	return (0);
+}
+
+int parsing_cmd(char **line) {
+	char quote;
+	int type;
+
+	quote = NULL;
+	if (ft_isquote(**line))
+		quote = *(*line)++;
+	if (!ft_strncmp(*line, "cd", 2)) {
+		type = TYPE_CD;
+		(*line) += 2;
+	}
+	else if (!ft_strncmp(*line, "pwd", 3)) {
+		type = TYPE_PWD;
+		(*line) += 3;
+	} else if (!ft_strncmp(*line, "echo", 4)) {
+		type = TYPE_ECHO;
+		(*line) += 4;
+	} else if (!ft_strncmp(*line, "export", 6)) {
+		type = TYPE_EXPORT;
+		(*line) += 6;
+	} else if (!ft_strncmp(*line, "env", 3)) {
+		type = TYPE_ENV;
+		(*line) += 3;
+	} else if (!ft_strncmp(*line, "unset", 5)) {
+		type = TYPE_UNSET;
+		(*line) += 5;
+	} else if (!ft_strncmp(*line, "exit", 4)) {
+		type = TYPE_EXIT;
+		(*line) += 4;
+	} else if (!ft_strncmp(*line, ">>", 2)) {
+		type = TYPE_DOUBLE_REDIRECT;
+		(*line) += 2;
+	} else if (!ft_strncmp(*line, ">", 1)) {
+		type = TYPE_REDIRECT;
+		(*line) += 1;
+	} else if (!ft_strncmp(*line, "|", 1)) {
+		type = TYPE_PIPE;
+		(*line) += 1;
+	} else if (!ft_strncmp(*line, ";", 1)) {
+		type = TYPE_SEMICOLON;
+		(*line) += 1;
+	} else {
+		type = WRONG_CMD;
+	}
+	return (type);
+}
+
+
 
 int parsing(t_minishell *ms) {
 	int ret;
+	int type;
 	char **line;
 
 	line = &ms->cmd_line;
 	while (*line && !(ret = 0)) {
 		while (ft_isspace(*line))
 			(*line)++;
-		//command파싱해야됨 ex) "env"
-		if ((ft_strncmp(*line, "cd", 2) == 0) && (!(*line)[2] || ft_isspace((*line)[2]))) {
+		if ((type = parsing_cmd(line)) < 0)
+			return (type);
+		if (type == TYPE_CD && (!(*line)[2] || ft_isspace((*line)[2]) || ft_isseparator((*line)[2]))) {
 			if ((ret = parsing_cd(ms->cmd, line)) < 0)
 				break;
 		}
-		else if ((ft_strncmp(*line, "pwd", 3) == 0) && (!(*line)[3] || ft_isspace((*line)[3]))) {
+		else if (type == TYPE_PWD && (!(*line)[3] || ft_isspace((*line)[3]) || ft_isseparator((*line)[3]))) {
 			if ((ret = parsing_pwd(ms->cmd, line)) < 0)
 				break;
 		}
-		else if ((ft_strncmp(*line, "echo", 4) == 0) && (!(*line)[4] || ft_isspace((*line)[4]))) {
+		else if (type == TYPE_ECHO && (!(*line)[4] || ft_isspace((*line)[4]) || ft_isseparator((*line)[4]))) {
 			if ((ret = parsing_echo(ms->cmd, line)) < 0)
 				break;
-		} else if ((ft_strncmp(*line, "export", 3) == 0) && (!(*line)[3] || ft_isspace((*line)[3]))) {
+		} else if (type == TYPE_EXPORT && (!(*line)[6] || ft_isspace((*line)[6]) || ft_isseparator((*line)[6]))) {
 			if ((ret = parsing_export(ms->cmd, line)) < 0)
 				break;
 		}
-		else if ((ft_strncmp(*line, "env", 3) == 0) && (!(*line)[3] || ft_isspace((*line)[3]))) {
+		else if (type == TYPE_ENV && (!(*line)[3] || ft_isspace((*line)[3]) || ft_isseparator((*line)[3]))) {
 			if ((ret = parsing_env(ms->cmd, line)) < 0)
 				break;
-		} else if ((ft_strncmp(*line, "unset", 5) == 0) && (!(*line)[5] || ft_isspace((*line)[5]))) {
+		} else if (type == TYPE_UNSET && (!(*line)[5] || ft_isspace((*line)[5]) || ft_isseparator((*line)[5]))) {
 			if ((ret = parsing_unset(ms->cmd, line)) < 0)
 				break;
-		} else if ((ft_strncmp(*line, "exit", 4) == 0) && (!(*line)[4] || ft_isspace((*line)[4]))) {
+		} else if (type == TYPE_EXIT && (!(*line)[4] || ft_isspace((*line)[4]) || ft_isseparator((*line)[4]))) {
+			if ((ret = parsing_exit(ms->cmd, line)) < 0)
+				break;
+		}
+		if (type == TYPE_REDIRECT && (!(*line)[1] || ft_isspace((*line)[1]))) {
+			if ((ret = parsing_redirect(ms->cmd, line)) < 0)
+				break;
+		} else if (type == TYPE_DOUBLE_REDIRECT && (!(*line)[2] || ft_isspace((*line)[2]))) {
+			if ((ret = parsing_double_redirect(ms->cmd, line)) < 0)
+				break;
+		} else if (type == TYPE_PIPE && (!(*line)[1] || ft_isspace((*line)[1]))) {
+			if ((ret = parsing_PIPE(ms->cmd, line)) < 0)
+				break;
+		} else if (type == TYPE_SEMICOLON && (!(*line)[4] || ft_isspace((*line)[4]))) {
 			if ((ret = parsing_exit(ms->cmd, line)) < 0)
 				break;
 		}
 	}
-	if (ret < 0)
-		cmd_error(ret);
-	return (0);
+	return (ret);
 }
 
 int main() {
@@ -391,7 +594,8 @@ int main() {
 	// parsing(ms.cmd, &l5);
 	// parsing(ms.cmd, &l6);
 	// parsing(ms.cmd, &l7);
-	parsing(&ms);
-	parsing(&ms);
+	int ret;
+	if ((ret = parsing(&ms)) < 0)
+		cmd_error(ret);
 	show(ms.cmd);
 }
